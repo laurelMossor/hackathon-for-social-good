e96b59efd79266f3b4621ba86121dc2ed3fb1f59
Laurel Mossor <laurel.mossor@gmail.com>
2022-06-25 13:12:12 -0700

    Simple site up and ready for content

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import (Flask, redirect, request, 
+            render_template, jsonify, flash, session)
+from jinja2 import StrictUndefined
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def homepage():
+
+    return render_template("homepage.html")
+
+
+
+###########################################################
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
+    # In debug mode, page will be updated when code is changed 
